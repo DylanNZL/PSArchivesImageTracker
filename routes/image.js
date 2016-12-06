f6f931bd8f8d3399db33e1c9ecc1d51b974c95ba
Dylan Cross <dylancross@me.com>
2016-12-06 18:55:18 +1300

    Changed to new query
    Mono made a new query to return all weapons which is now used, also
    converts negative id’s to the positive so the psarchives queries work

--- routes/image.js
+++ routes/image.js
@@ -6,10 +6,12 @@ var Q           = require('q');
 var template = JSON.stringify({
     id : 0,
     name : null,
+    image_id : null,
     census : null,
     psarchives : null
 });
 
+// Stores each instantiation of template in here
 var weap = [];
 
 /* GET home page. */
@@ -17,8 +19,9 @@ router.get('/', function(req, res, next) {
     render(res);
 });
 
+// fills the array and then renders the page
 function render(res) {
-    getData("mononz").then(function (result) {
+    getData().then(function (result) {
         if (result) {
             res.render('image', { title: 'PSArchives Images', weapon: weap });
         } else {
@@ -27,17 +30,32 @@ function render(res) {
     });
 }
 
-function getData(char) {
+function abs(x) {
+    if (x < 0) {
+        return -x;
+    }
+    return x;
+}
+
+// Fetches the weapons for a specified character and stores them in the weap array - possible to add multiple characters
+function getData() {
     var response = Q.defer();
-    var url = "http://psarchives.com/weapons_list?name=" + char;
+    var url = "http://psarchives.com/v1/update/items";
+
     prequest(url).then(function (body) {
-        console.log(body.weapons);
-        body.weapons.forEach(function(wep) {
+        // Debug only
+        // console.log(body);
+        body.forEach(function(wep) {
             var obj = JSON.parse(template);
-                obj.id = wep.information.item_id;
-                obj.name = wep.information.name_en;
-                obj.census = "https://census.daybreakgames.com/files/ps2/images/static/" + wep.information.image_id;
-                obj.psarchives = "https://storage.googleapis.com/planetside/static/" + wep.information.image_id + ".png";
+                obj.id = wep.item_id;
+                obj.name = wep.name;
+                obj.image_id = wep.image_id;
+                obj.census = "https://census.daybreakgames.com/files/ps2/images/static/" + Math.abs(wep.image_id);
+                if (wep.image_id == -85785) {
+                    console.error(abs(wep.image_id));
+                    console.error(wep.image_id);
+                }
+                obj.psarchives = "https://storage.googleapis.com/planetside/static/" + Math.abs(wep.image_id) + ".png";
             if (obj.id > 0) {
                 if (!weap[obj.id]) {
                     weap[obj.id] = obj;
